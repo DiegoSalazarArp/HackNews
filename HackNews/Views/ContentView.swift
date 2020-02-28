135a89bd342f8023b8ae17b303988716c4d7f6a9
Diego Salazar Arp <d.salazararp@gmail.com>
2020-02-28 17:10:12 -0300

    Add gif

---
+++ HackNews/Views/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  HackNews
+//
+//  Created by Diego Salazar Arp on 15-01-20.
+//  Copyright © 2020 Diego Salazar Arp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts) { post in
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                }
+                
+            }
+            .navigationBarTitle("HACKER NEWS")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+
+
